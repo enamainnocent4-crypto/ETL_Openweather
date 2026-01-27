18a0393d58fac62ab4337b605ae28268c2cd2f9a
Enama <enamainnocent4@gmail.com>
2026-01-27 04:12:18 +0100

    main.py

--- main.py
+++ main.py
@@ -31,3 +31,46 @@ else:
     
     df = pd.json_normalize(data)
     print(df)
+    
+    
+    
+def transform_openweather(df):
+    df = df.copy()
+    if "weather" in df.columns:
+        df["weather_main"] = df["weather"].str[0].str["main"]
+        df["weather_description"] = df["weather"].str[0].str["description"]
+
+        df.drop(columns=["weather"], inplace=True)
+        
+    cols_drop = [
+        "base",
+        "cod",
+        "id",
+        "timezone",
+        "main.sea_level",
+        "main.grnd_level",
+        "main.temp_min",
+        "main.temp_max",
+        "wind.deg",
+        "wind.gust"
+    ]
+    df.drop(columns=[c for c in cols_drop if c in df.columns], inplace=True)
+    df.rename(columns={
+        "coord.lon": "Longitude",
+        "coord.lat": "Latitude",
+        "main.humidity": "humidity",
+        "main.pressure": "pressure",
+        "clouds.all": "cloudiness",
+        "dt": "datetime",
+        "wind.speed": "wind_speed",
+        "name": "city",
+        "main.temp": "temperature",
+        "main.feels_like": "feels_like",
+        "visibility": "visibility",
+        "weather_description": "description"
+    }, inplace=True)
+
+    if "datetime" in df.columns:
+        df["datetime"] = pd.to_datetime(df["datetime"], unit="s")
+
+    return df
